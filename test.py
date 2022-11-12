119ff4a5f3a52bbbf1addb83ba4a6f8a4d0258f2
Hamza ENNAJI <hamzaonbinance@gmail.com>
2022-11-12 14:15:01 +0100

    Modification on function

--- test.py
+++ test.py
@@ -3,26 +3,14 @@ import pandas as pd
 import xml.etree.ElementTree as ET
 from datetime import datetime
 import os
-import wget
-import urllib
-
-url = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-PY0221EN-SkillsNetwork/labs/module%206/Lab%20-%20Extract%20Transform%20Load/data/source.zip"
-
 
 # CSV Extract Function
 def extract_from_csv(file_to_process):
     dataframe = pd.read_csv(file_to_process)
     return dataframe
-
-
-# JSON Extract Function
 def extract_from_json(file_to_process):
-    dataframe = pd.read_json(file_to_process,lines =True)
+    dataframe = pd.read_json(file_to_process, lines = True)
     return dataframe
-
-
-
-# XML Extract Function
 def extract_from_xml(file_to_process):
     dataframe = pd.DataFrame(columns=["name", "height", "weight"])
     tree = ET.parse(file_to_process)
@@ -33,10 +21,6 @@ def extract_from_xml(file_to_process):
         weight = float(person.find("weight").text)
         dataframe = dataframe.append({"name": name, "height": height, "weigth": weight}, ignore_index=True)
     return dataframe
-
-######################### extract #########################
-
-
 def extract():
     extracted_data = pd.DataFrame(columns=["name", "height", "weight"])
     # process all csv files
@@ -55,10 +39,6 @@ def extract():
         extracted_data = extracted_data.append(extract_from_xml(xmlfile), ignore_index=True)
 
     return extracted_data
-
-    ######################### transform #########################
-
-
 def transform(data):
     # Convert height which is in inches to millimeter
     # Convert the datatype of the column into float
@@ -71,14 +51,12 @@ def transform(data):
     # data.weight = data.weight.astype(float)
     # Convert pounds to kilograms and round off to two decimals(one pound is 0.45359237 kilograms)
     data['weight'] = round(data.weight * 0.45359237, 2)
+    print(data)
     return data
 
-
-    ######################### transform #########################
 def load(targetfile, data_to_load):
     data_to_load.to_csv(targetfile)
 
-    ######################### logging #########################
 def log(message):
     timestamp_format = '%Y-%h-%d-%H:%M:%S'  # Year-Monthname-Day-Hour-Minute-Second
     now = datetime.now()  # get current timestamp
@@ -86,12 +64,16 @@ def log(message):
     with open("logfile.txt", "a") as f:
         f.write(timestamp + ',' + message + '\n')
 
-    ######################### Running ETL Process #########################
-print("begin the process")
+
+data = extract()
+print(data)
+#print(transform(data))
+
+load("transformed.csv", data)
+
 log("ETL Job Started")
 log("Extract phase Started")
 extracted_data = extract()
-print("hahaaaaaaaaaaaaaaaaaaaaaaaahahahaha")
 
 log("Extract phase Ended")
 extracted_data
